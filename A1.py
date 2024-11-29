80954698829d6ed5fe28fdb88b9260e9327598ad
Ashmitha T S <ashmitha.s@s.amity.edu>
2024-11-29 16:28:43 +0530

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 14:33:56 2024
+
+@author: ashmi
+"""
+
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+c=int(input("Enter the third number : "))
+if((a>b) & (a>c) & (b>c)):
+    print(a,"is the maximum number and",c,"is the minimum number")
+elif((a>b) & (a>c) & (c>b)):
+    print(a,"is the maximum number and",b,"is the minimum number")
+elif((b>a) & (b>c) & (a>c)):
+    print(b,"is the maximum number and",c,"is the minimum number") 
+elif((b>a) & (b>c) & (c>a)):
+    print(b,"is the maximum number and",a,"is the minimum number")
+elif((c>a) & (c>b) & (a>b)):
+    print(c,"is the maximum number and",b,"is the minimum number")
+else:
+    print(c,"is the maximum number and",a,"is the minimum number")
